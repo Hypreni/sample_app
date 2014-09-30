0abfe6b93ae932ee0ab71d688d5ed19b2113b464
Geoffrey L.A. Dobbs <dobbs.geoffrey@gmail.com>
2014-09-30 20:58:39 +0200

    add home page test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "Static pages" do
 
   describe "Home page" do
 
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
     it "should have the base title" do
       visit '/static_pages/home'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
