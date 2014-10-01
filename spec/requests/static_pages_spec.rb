11e12276c8d3155373f89e7875f65e06fd1abec6
Geoffrey L.A. Dobbs <dobbs.geoffrey@gmail.com>
2014-10-01 18:40:11 +0200

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,18 +6,17 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    
-    it {should have_content('Sample App')}
-    it {should have_title(full_title(''))}
-    it {should_not have_title('| Home')}
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-    before { visit help_path}
-    
-    it {should have_content('Help')}
-    it {should have_title(full_title("Help"))}
-    end
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
